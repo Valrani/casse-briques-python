aa14807d0ea371d535383c716e7c7be9ecb604d0
antoinederoose <antoinederoose@yahoo.fr>
2022-02-16 17:38:18 +0100

    bar moving wip

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 from tkinter import *
 
 
+def initBar():
+    obj = can.create_rectangle(
+        CANVAS_WIDTH / 2 - barSize / 2,
+        CANVAS_HEIGHT - 100,
+        CANVAS_WIDTH / 2 + barSize / 2,
+        CANVAS_HEIGHT - 90,
+        fill="white"
+    )
+    can.bind('<Motion>', onBarMoving)
+    print("Bar created with coordinates", can.coords(obj))
+    return obj
+
+
+def onBarMoving(event):
+    can.coords(
+        barId,
+        event.x - barSize / 2,
+        CANVAS_HEIGHT - 100,
+        event.x + barSize / 2,
+        CANVAS_HEIGHT - 90
+    )
+
+
 def initBall():
-    return can.create_oval(
+    obj = can.create_oval(
         CANVAS_WIDTH / 2 - 5,
         CANVAS_HEIGHT / 2 - 5,
         CANVAS_WIDTH / 2 + 5,
         CANVAS_HEIGHT / 2 + 5,
         fill="white"
     )
+    print("Ball created with coordinates", can.coords(obj))
+    return obj
 
 
 def moveBall():
@@ -31,13 +56,14 @@ def moveBall():
 # constants
 CANVAS_WIDTH = 500
 CANVAS_HEIGHT = 600
-
 BALL_DIRECTION_LEFT = -5
 BALL_DIRECTION_RIGHT = 5
 BALL_DIRECTION_UP = -5
 BALL_DIRECTION_DOWN = 5
 
 # variables
+barId = None
+barSize = 100
 ballId = None
 ballSpeed = 15
 ballHorizontalDirection = BALL_DIRECTION_LEFT
@@ -50,6 +76,7 @@ fen.resizable(False, False)
 can = Canvas(fen, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="black")
 can.pack()
 
+barId = initBar()
 ballId = initBall()
 
 #moveBall()
