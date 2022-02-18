9d77c67741bb2804fdbe2c6f2ddb13015389d643
antoinederoose <antoinederoose@yahoo.fr>
2022-02-18 20:23:40 +0100

    init level 1

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from tkinter import *
 
 
+def initLevel1():
+    for x1 in range (25, 425, 50):
+        if x1 not in [175, 225]:
+            bricks.append(can.create_rectangle(x1, 25, x1 + 25, 50, fill=BRICK_STRENGTH_1_COLOR))
+    for x1 in range(25, 425, 50):
+        if x1 not in [175, 225]:
+            bricks.append(can.create_rectangle(x1, 125, x1 + 25, 150, fill=BRICK_STRENGTH_2_COLOR))
+    for x1 in range(25, 425, 50):
+        if x1 not in [175, 225]:
+            bricks.append(can.create_rectangle(x1, 175, x1 + 25, 200, fill=BRICK_STRENGTH_1_COLOR))
+    for x1 in range(175, 275, 50):
+        bricks.append(can.create_rectangle(x1, 325, x1 + 25, 350, fill=BRICK_STRENGTH_1_COLOR))
+
+
 def initBar():
     obj = can.create_rectangle(
         CANVAS_WIDTH / 2 - barSize / 2,
@@ -55,7 +69,7 @@ def moveBall():
     # collisions with bar
     if ballCoords[3] == CANVAS_HEIGHT - BAR_OFFSET_FROM_BOTTOM - 10 and ballVerticalDirection == BALL_DIRECTION_DOWN:
         barCoords = can.coords(barId)
-        if barCoords[0] <= ballCoords[2] <= barCoords[2]:
+        if barCoords[0] - 5 <= ballCoords[2] <= barCoords[2] + 5:
             ballVerticalDirection = BALL_DIRECTION_UP
     # move the ball
     can.move(ballId, ballHorizontalDirection, ballVerticalDirection)
@@ -63,13 +77,15 @@ def moveBall():
 
 
 # constants - do not change during the execution !
-CANVAS_WIDTH = 500
-CANVAS_HEIGHT = 600
+CANVAS_WIDTH = 425
+CANVAS_HEIGHT = 700
 BAR_OFFSET_FROM_BOTTOM = 100
 BALL_DIRECTION_LEFT = -5
 BALL_DIRECTION_RIGHT = 5
 BALL_DIRECTION_UP = -5
 BALL_DIRECTION_DOWN = 5
+BRICK_STRENGTH_1_COLOR = "yellow"
+BRICK_STRENGTH_2_COLOR = "green"
 
 # variables - some values can be changed to tweak the gameplay
 barId = None
@@ -78,16 +94,20 @@ ballId = None
 ballSpeed = 10
 ballHorizontalDirection = BALL_DIRECTION_LEFT
 ballVerticalDirection = BALL_DIRECTION_UP
+bricks = []     # remaining bricks in the current level
 
 # GUI
 fen = Tk()
 fen.title("Balle qui bouge")
 fen.resizable(False, False)
 can = Canvas(fen, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="black")
+backgroundImage = PhotoImage(file="images/bg1.gif")
+can.create_image(0, 0, image=backgroundImage)
 can.pack()
 
 barId = initBar()
 ballId = initBall()
 moveBall()
+initLevel1()
 
 fen.mainloop()
