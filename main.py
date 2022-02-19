d7ec80adc8b2a40aa2c83433a1b30cbca0a8506a
antoinederoose <antoinederoose@yahoo.fr>
2022-02-19 19:06:18 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 from tkinter import *
 
+"""""""""""""""""""""""""""""""""""""""
+""""""""""""" CONSTANTS """"""""""""""
+"""""""""""""""""""""""""""""""""""""""
+
+
+CANVAS_WIDTH = 425
+CANVAS_HEIGHT = 700
+BAR_OFFSET_FROM_BOTTOM = 100
+BALL_DIRECTION_LEFT = -5
+BALL_DIRECTION_RIGHT = 5
+BALL_DIRECTION_UP = -5
+BALL_DIRECTION_DOWN = 5
+BRICK_STRENGTH_1_COLOR = "yellow"
+BRICK_STRENGTH_2_COLOR = "green"
+BRICK_STRENGTH_3_COLOR = "blue"
+X1 = 0
+Y1 = 1
+X2 = 2
+Y2 = 3
+
+
+"""""""""""""""""""""""""""""""""""""""""
+""""""""""""" GAME METHODS """"""""""""""
+"""""""""""""""""""""""""""""""""""""""""
+
 
 def initBar():
     return cans[currentCan].create_rectangle(
@@ -36,52 +61,52 @@ def initBall():
 
 
 def moveBall():
-    if stopMoveBall:
+    if ballStop:
         return
     checkBallCollisionsWithWalls()
     checkBallCollisionsWithBar()
     checkBallCollisionsWithBricks()
-    cans[currentCan].move(ballId, ballHorizontalDirection, ballVerticalDirection)
+    cans[currentCan].move(ballId, ballDirectionH, ballDirectionV)
     win.after(ballSpeed, moveBall)
 
 
 def checkBallCollisionsWithWalls():
-    global ballHorizontalDirection, ballVerticalDirection
+    global ballDirectionH, ballDirectionV
     ballCoords = cans[currentCan].coords(ballId)
     if ballCoords[X1] <= 0:
-        ballHorizontalDirection = BALL_DIRECTION_RIGHT
+        ballDirectionH = BALL_DIRECTION_RIGHT
     if ballCoords[Y1] <= 0:
-        ballVerticalDirection = BALL_DIRECTION_DOWN
+        ballDirectionV = BALL_DIRECTION_DOWN
     if ballCoords[X2] >= CANVAS_WIDTH:
-        ballHorizontalDirection = BALL_DIRECTION_LEFT
+        ballDirectionH = BALL_DIRECTION_LEFT
     if ballCoords[Y2] >= CANVAS_HEIGHT:
-        ballVerticalDirection = BALL_DIRECTION_UP
+        ballDirectionV = BALL_DIRECTION_UP
 
 
 def checkBallCollisionsWithBar():
-    global ballVerticalDirection
+    global ballDirectionV
     ballCoords = cans[currentCan].coords(ballId)
     if barId in cans[currentCan].find_overlapping(ballCoords[X1], ballCoords[Y1], ballCoords[X2], ballCoords[Y2]):
-        ballVerticalDirection = BALL_DIRECTION_UP
+        ballDirectionV = BALL_DIRECTION_UP
 
 
 def checkBallCollisionsWithBricks():
-    global ballHorizontalDirection, ballVerticalDirection
+    global ballDirectionH, ballDirectionV
     ballCoords = cans[currentCan].coords(ballId)
     for brickId in bricks:
         if brickId in cans[currentCan].find_overlapping(ballCoords[X1], ballCoords[Y1], ballCoords[X2], ballCoords[Y2]):
             brickCoords = cans[currentCan].coords(brickId)
             if ballCoords[X2] == brickCoords[X1]:
-                ballHorizontalDirection = BALL_DIRECTION_LEFT
+                ballDirectionH = BALL_DIRECTION_LEFT
                 hitBrick(brickId)
             if ballCoords[Y2] == brickCoords[Y1]:
-                ballVerticalDirection = BALL_DIRECTION_UP
+                ballDirectionV = BALL_DIRECTION_UP
                 hitBrick(brickId)
             if ballCoords[X1] == brickCoords[X2]:
-                ballHorizontalDirection = BALL_DIRECTION_RIGHT
+                ballDirectionH = BALL_DIRECTION_RIGHT
                 hitBrick(brickId)
             if ballCoords[Y1] == brickCoords[Y2]:
-                ballVerticalDirection = BALL_DIRECTION_DOWN
+                ballDirectionV = BALL_DIRECTION_DOWN
                 hitBrick(brickId)
 
 
@@ -95,36 +120,27 @@ def hitBrick(brickId):
         cans[currentCan].delete(brickId)
 
 
-# constants - do not change during the execution !
-CANVAS_WIDTH = 425
-CANVAS_HEIGHT = 700
-BAR_OFFSET_FROM_BOTTOM = 100
-BALL_DIRECTION_LEFT = -5
-BALL_DIRECTION_RIGHT = 5
-BALL_DIRECTION_UP = -5
-BALL_DIRECTION_DOWN = 5
-BRICK_STRENGTH_1_COLOR = "yellow"
-BRICK_STRENGTH_2_COLOR = "green"
-BRICK_STRENGTH_3_COLOR = "blue"
-X1 = 0
-Y1 = 1
-X2 = 2
-Y2 = 3
+"""""""""""""""""""""""""""""""""""""""
+""""""""""""" VARIABLES """"""""""""""
+"""""""""""""""""""""""""""""""""""""""
 
-# variables - some values can be changed to tweak the gameplay
-backgroundImage = None
+
 barId = None
 barSize = 100
 ballId = None
 ballSpeed = 10
-ballHorizontalDirection = BALL_DIRECTION_LEFT
-ballVerticalDirection = BALL_DIRECTION_UP
-bricks = []  # remaining bricks in the current level
-stopMoveBall = None
+ballDirectionH = BALL_DIRECTION_LEFT
+ballDirectionV = BALL_DIRECTION_UP
+# True to stop the execution of moveBall()
+ballStop = None
+# remaining bricks in the current level
+bricks = []
+# the canvas, where cans[0] is the level 1 canvas, [1] is the level 2 canvas etc.
 cans = []
+# same thing but with the canvas background images
 backgroundImages = []
+# the current canvas displayed, i.e. the position in cans
 currentCan = None
-
 # GUI
 win = Tk()
 win.title("Casse-briques")
@@ -134,11 +150,13 @@ level1Frame = None
 level2Frame = None
 
 
+"""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""" FRAMES AND LEVELS MANAGEMENT """"""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
 def startLevel1():
-    global currentCan, barId, ballId, stopMoveBall
-    currentCan = 0
-    cans[currentCan].create_image(0, 0, image=backgroundImages[currentCan])
-    # create bricks
+    setupLevel(0)
     for x1 in range(25, 425, 50):
         if x1 not in [175, 225]:
             bricks.append(cans[currentCan].create_rectangle(x1, 25, x1 + 25, 50, fill=BRICK_STRENGTH_1_COLOR))
@@ -150,21 +168,11 @@ def startLevel1():
             bricks.append(cans[currentCan].create_rectangle(x1, 175, x1 + 25, 200, fill=BRICK_STRENGTH_1_COLOR))
     for x1 in range(175, 275, 50):
         bricks.append(cans[currentCan].create_rectangle(x1, 325, x1 + 25, 350, fill=BRICK_STRENGTH_1_COLOR))
-    # create bar and ball
-    barId = initBar()
-    cans[currentCan].bind('<Motion>', onBarMoving)
-    ballId = initBall()
-    stopMoveBall = False
-    moveBall()
-    # display the frame
     level1Frame.tkraise()
 
 
 def startLevel2():
-    global currentCan, barId, ballId, stopMoveBall
-    currentCan = 1
-    cans[currentCan].create_image(0, 0, image=backgroundImages[currentCan])
-    # create bricks
+    setupLevel(1)
     for x1 in range(25, 425, 50):
         if x1 not in [175, 225]:
             bricks.append(cans[currentCan].create_rectangle(x1, 25, x1 + 25, 50, fill=BRICK_STRENGTH_2_COLOR))
@@ -175,14 +183,30 @@ def startLevel2():
     for x1 in range(175, 275, 50):
         if x1 not in [175, 225]:
             bricks.append(cans[currentCan].create_rectangle(x1, 325, x1 + 25, 350, fill=BRICK_STRENGTH_2_COLOR))
-    # create bar and ball
+    level2Frame.tkraise()
+
+
+def setupLevel(lvlIndice):
+    """ Initialize the background image, bar and ball for the current canvas. """
+    global currentCan, barId, ballId, ballStop
+    currentCan = lvlIndice
+    cans[currentCan].create_image(0, 0, image=backgroundImages[currentCan])
     barId = initBar()
     cans[currentCan].bind('<Motion>', onBarMoving)
     ballId = initBall()
-    stopMoveBall = False
+    ballStop = False
     moveBall()
-    # display the frame
-    level2Frame.tkraise()
+
+
+def exitLevel(event):
+    """ Clean the canvas, stop and reset the ball, display the home frame. """
+    global ballStop, ballDirectionH, ballDirectionV
+    ballStop = True
+    cans[currentCan].delete('all')
+    bricks.clear()
+    ballDirectionH = BALL_DIRECTION_LEFT
+    ballDirectionV = BALL_DIRECTION_UP
+    homeFrame.tkraise()
 
 
 """ HOME FRAME """
@@ -191,12 +215,15 @@ homeFrame.grid(row=0, column=0)
 homeFrame.pack_propagate(False)
 titleLbl = Label(homeFrame, text="CASSE-BRIQUES", fg="black", font="Andale 30 bold")
 titleLbl.pack(pady=50)
-startLvl1Btn = Button(homeFrame, text="Niveau 1", highlightbackground="black", activeforeground="gray", command=startLevel1)
+startLvl1Btn = Button(homeFrame, text="Niveau 1", highlightbackground="black", activeforeground="gray",
+                      command=startLevel1)
 startLvl1Btn.pack()
-startLvl2Btn = Button(homeFrame, text="Niveau 2", highlightbackground="black", activeforeground="gray", command=startLevel2)
+startLvl2Btn = Button(homeFrame, text="Niveau 2", highlightbackground="black", activeforeground="gray",
+                      command=startLevel2)
 startLvl2Btn.pack()
 exitBtn = Button(homeFrame, text="Quitter", highlightbackground="black", activeforeground="gray", command=win.destroy)
 exitBtn.pack()
+
 
 """ LEVEL 1 FRAME """
 level1Frame = Frame(win, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
@@ -207,6 +234,7 @@ backgroundImage = PhotoImage(file="images/bg1.gif")
 can.pack()
 cans.append(can)
 backgroundImages.append(backgroundImage)
+
 
 """ LEVEL 2 FRAME """
 level2Frame = Frame(win, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
@@ -219,17 +247,11 @@ cans.append(can)
 backgroundImages.append(backgroundImage)
 
 
-def exitLevel(event):
-    global stopMoveBall, ballHorizontalDirection, ballVerticalDirection
-    stopMoveBall = True
-    cans[currentCan].delete('all')
-    bricks.clear()
-    ballHorizontalDirection = BALL_DIRECTION_LEFT
-    ballVerticalDirection = BALL_DIRECTION_UP
-    homeFrame.tkraise()
+"""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""""""""""""" SOME MORE THINGS """"""""""""""""
+"""""""""""""""""""""""""""""""""""""""""""""""""""
 
 
 win.bind('<Escape>', exitLevel)
-# launch the game
 homeFrame.tkraise()
 win.mainloop()
